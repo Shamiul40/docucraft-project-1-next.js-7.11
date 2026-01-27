54ec4d45414a5c5758c18d99c630651b0dbb7094
Shamiul40 <shamiul4040@gmail.com>
2026-01-27 22:09:06 +0600

    root and nonroot separate

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -7,7 +7,17 @@ export default function Header({docs}) {
         // console.log(docs)
 
         const root = docs.filter(doc=> !doc.parent)
-        console.log("there is root console", root)
+        // console.log("there is root console", root)
+
+        const nonRoot = Object.groupBy(
+            docs.filter(doc=>doc.parent), 
+            (doc)=>{
+                const parent = doc.parent
+               
+                return parent
+            } 
+        )
+        console.log(nonRoot)
 
   return (
     <div>
