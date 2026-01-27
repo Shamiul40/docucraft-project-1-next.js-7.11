4804596affcd097aee5865fd25117a9e1135c68e
Shamiul40 <shamiul4040@gmail.com>
2026-01-28 00:48:38 +0600

    edit

--- app/docs/[contentId]/page.js
+++ app/docs/[contentId]/page.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function ContentIdPage({params : {contentId}}) {
   return (
     <div>
+        <p>content : {contentId}</p>
       {contentId}
     </div>
   )
