8e67b9073ee38c5de53104cc9ed53dcb91b915a6
Shamiul40 <shamiul4040@gmail.com>
2026-01-27 22:16:57 +0600

    bug fix

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -1,30 +1,38 @@
-import React from 'react'
-import Logo from './Logo'
-import Search from './Search'
+import React from "react";
+import Logo from "./Logo";
+import Search from "./Search";
 
-export default function Header({docs}) {
+export default function Header({ docs }) {
+  // console.log(docs)
 
-        // console.log(docs)
+  const root = docs.filter((doc) => !doc.parent);
+  // console.log("there is root console", root)
 
-        const root = docs.filter(doc=> !doc.parent)
-        // console.log("there is root console", root)
+  const nonRoot = Object.groupBy(
+    docs.filter((doc) => doc.parent),
+    (doc) => {
+      const parent = doc.parent;
 
-        const nonRoot = Object.groupBy(
-            docs.filter(doc=>doc.parent), 
-            (doc)=>{
-                const parent = doc.parent
-               
-                return parent
-            } 
-        )
-        console.log(nonRoot)
+      return parent;
+    },
+  );
+  console.log(nonRoot);
 
   return (
     <div>
       <Logo></Logo>
 
+      <div>
+        <ul>
+          {
+            root && root.map(rootNode=>(
+                <li key={rootNode.id}>{rootNode.title}</li>
+            ))
+          }
+        </ul>
+      </div>
 
       <Search></Search>
     </div>
-  )
+  );
 }
