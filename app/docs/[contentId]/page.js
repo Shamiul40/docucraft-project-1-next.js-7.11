00a542ec8935e096599ec4c476af6000d3edc5b5
Shamiul40 <shamiul4040@gmail.com>
2026-01-28 23:32:20 +0600

    ui

--- app/docs/[contentId]/page.js
+++ app/docs/[contentId]/page.js
@@ -2,29 +2,37 @@ import Tag from "@/app/Components/Tag";
 import { getDocumentsContent } from "@/lib/doc";
 import Link from "next/link";
 
-
 export default async function ContentIdPage({ params }) {
   const { contentId } = await params;
 
-  const getAllDocumentsContent = await getDocumentsContent(contentId)
+  const getAllDocumentsContent = await getDocumentsContent(contentId);
 
   return (
     <div>
-        <h1>introduction</h1>
-        <div>
-          <p> 
-             published on : {getAllDocumentsContent.date} by: <Link href={`/author/${getAllDocumentsContent.author}`}>{getAllDocumentsContent.author}</Link> under the <Link href={`/category/${getAllDocumentsContent.category}`}>{getAllDocumentsContent.category}</Link> category
-          </p>
-          <div className="flex gap-4">
-            {getAllDocumentsContent.tags.map(tag=><Tag key={tag} tag={tag}></Tag>)
-            
-            }
-          </div>
-      
+      <h1>introduction</h1>
+      <div>
+        <p>
+          published on : {getAllDocumentsContent.date} by:{" "}
+          <Link href={`/author/${getAllDocumentsContent.author}`}>
+            {getAllDocumentsContent.author}
+          </Link>{" "}
+          under the{" "}
+          <Link href={`/category/${getAllDocumentsContent.category}`}>
+            {getAllDocumentsContent.category}
+          </Link>{" "}
+          category
+        </p>
+        <div className="flex gap-4">
+          {getAllDocumentsContent.tags.map((tag) => (
+            <Tag key={tag} tag={tag}></Tag>
+          ))}
         </div>
-        <div>
-
-        </div>
+      </div>
+      <div 
+        dangerouslySetInnerHTML={{__html :getAllDocumentsContent.contentHtml}}
+        >
+       
+      </div>
     </div>
   );
 }
