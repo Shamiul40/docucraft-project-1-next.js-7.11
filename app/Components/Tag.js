3cb4a69aceeff7b024c24a7f7a87010c4d44c450
Shamiul40 <shamiul4040@gmail.com>
2026-01-28 23:26:29 +0600

    tag

---
+++ app/Components/Tag.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Tag({tag}) {
+    console.log(tag)
+  return (
+    <div >
+      <span>{tag}</span>
+    </div>
+  )
+}
--- app/docs/[contentId]/page.js
+++ app/docs/[contentId]/page.js
@@ -1,3 +1,4 @@
+import Tag from "@/app/Components/Tag";
 import { getDocumentsContent } from "@/lib/doc";
 import Link from "next/link";
 
@@ -14,6 +15,11 @@ export default async function ContentIdPage({ params }) {
           <p> 
              published on : {getAllDocumentsContent.date} by: <Link href={`/author/${getAllDocumentsContent.author}`}>{getAllDocumentsContent.author}</Link> under the <Link href={`/category/${getAllDocumentsContent.category}`}>{getAllDocumentsContent.category}</Link> category
           </p>
+          <div className="flex gap-4">
+            {getAllDocumentsContent.tags.map(tag=><Tag key={tag} tag={tag}></Tag>)
+            
+            }
+          </div>
       
         </div>
         <div>
