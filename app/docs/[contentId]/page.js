8c3a9699752a8d3ee067caf2ddc79314a60bcb0d
Shamiul40 <shamiul4040@gmail.com>
2026-01-28 23:21:30 +0600

    pathe create

--- app/docs/[contentId]/page.js
+++ app/docs/[contentId]/page.js
@@ -1,4 +1,5 @@
 import { getDocumentsContent } from "@/lib/doc";
+import Link from "next/link";
 
 
 export default async function ContentIdPage({ params }) {
@@ -11,9 +12,12 @@ export default async function ContentIdPage({ params }) {
         <h1>introduction</h1>
         <div>
           <p> 
-             published on : {getAllDocumentsContent.date} by: {getAllDocumentsContent.author}
+             published on : {getAllDocumentsContent.date} by: <Link href={`/author/${getAllDocumentsContent.author}`}>{getAllDocumentsContent.author}</Link> under the <Link href={`/category/${getAllDocumentsContent.category}`}>{getAllDocumentsContent.category}</Link> category
           </p>
       
+        </div>
+        <div>
+
         </div>
     </div>
   );
