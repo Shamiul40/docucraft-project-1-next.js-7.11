ca828b04cab0f44bced97b448b15f39fb1994ec1
Shamiul40 <shamiul4040@gmail.com>
2026-01-29 22:29:59 +0600

    chagne

--- app/Components/SideBar.js
+++ app/Components/SideBar.js
@@ -1,49 +1,47 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-import { getDocumentsByAuthor, getDocumentsByCategory, getDocumentsByTag } from "../utils/docs-utils";
+import { useEffect, useState } from "react";
+import {
+  getDocumentsByAuthor,
+  getDocumentsByCategory,
+  getDocumentsByTag,
+} from "../utils/docs-utils";
 
 const Sidebar = ({ docs }) => {
+  const pathName = usePathname();
 
-    const pathName = usePathname()
+  const [rootNodes, setRootNodes]= useState([])
+  const [nonRootNodesGrouped, setNonRootNodesGrouped] = useState({});
 
-    useEffect(()=>{
+  useEffect(() => {
+    let matchedDocs = docs;
 
-      let matchedDocs = docs;
+    if (pathName.includes("/tags")) {
+      const tag = pathName.split("/")[2];
 
-      if(pathName.includes("/tags")) {
+      matchedDocs = getDocumentsByTag(docs, tag);
+    } else if (pathName.includes("/author")) {
+      const author = pathName.split("/")[2];
 
-       const tag= pathName.split("/") [2]
+      matchedDocs = getDocumentsByAuthor(docs, author);
+    } else if (pathName.includes("/category")) {
+      const category = pathName.split("/")[2];
 
-       matchedDocs = getDocumentsByTag(docs, tag)
+      matchedDocs = getDocumentsByCategory(docs, category);
+    }
 
-      } else if (pathName.includes("/author")) {
+    const roots = matchedDocs.filter((doc) => !doc.parent);
 
-        const author = pathName.split("/") [2] 
+    const nonRoots = Object.groupBy(
+      matchedDocs.filter((doc) => doc.parent),
+      ({ parent }) => parent,
+    );
 
-        matchedDocs = getDocumentsByAuthor(docs, author)
-
-      } else if (pathName.includes("/category")) {
-
-        const category = pathName.split("/") [2]
-
-        matchedDocs =getDocumentsByCategory(docs, category)
-
-
-      }
-
-    },[pathName])
-
-
-  const roots = docs.filter((doc) => !doc.parent);
-  console.log({ roots });
-
-  const nonRoots = Object.groupBy(
-    docs.filter((doc) => doc.parent),
-    ({ parent }) => parent,
-  );
-  console.log({ nonRoots });
+    setRootNodes(roots)
+    setNonRootNodesGrouped(nonRoots)
+    
+  }, [pathName, docs]);
 
   return (
     <nav className="lg:block my-10">
@@ -53,7 +51,7 @@ const Sidebar = ({ docs }) => {
           <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
           <div className="absolute left-2 h-6 w-px bg-emerald-500"></div>
           <ul role="list" className="border-l border-transparent">
-            {roots.map((rootNode) => (
+            {rootNodes.map((rootNode) => (
               <li key={rootNode.id} className="relative">
                 <Link
                   aria-current="page"
@@ -62,9 +60,9 @@ const Sidebar = ({ docs }) => {
                 >
                   <span className="truncate">{rootNode.title}</span>
                 </Link>
-                {nonRoots[rootNode.id] && (
+                {nonRootNodesGrouped[rootNode.id] && (
                   <ul role="list" className="border-l border-transparent">
-                    {nonRoots[rootNode.id].map((subRoot) => (
+                    {nonRootNodesGrouped[rootNode.id].map((subRoot) => (
                       <li key={subRoot.id}>
                         <Link
                           className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
